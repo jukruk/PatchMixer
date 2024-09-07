c5b5bb8099631d7afda8979f81a2375abd56cf6d
jukruk <juliuszkruk@gmail.com>
2024-09-07 17:50:52 +0200

    Added training scripts and result file

--- scripts/PatchTST/air.sh
+++ scripts/PatchTST/air.sh
@@ -6,40 +6,38 @@ if [ ! -d "./logs/LongForecasting" ]; then
     mkdir ./logs/LongForecasting
 fi
 seq_len=336
-model_name=PatchMixer
+model_name=PatchTST
 
-root_path_name=./dataset/
-data_path_name=electricity.csv
-model_id_name=Electricity
+root_path_name=../data/air_quality/
+data_path_name=air_s.csv
+model_id_name=air_s
 data_name=custom
 
 random_seed=2021
 
-for pred_len in 96 192 336 720
+for pred_len in 96 192 336
 do
     python -u run_longExp.py \
       --random_seed $random_seed \
       --is_training 1 \
       --root_path $root_path_name \
       --data_path $data_path_name \
-      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model_id $model_id_name \
       --model $model_name \
       --data $data_name \
       --features M \
       --seq_len $seq_len \
       --pred_len $pred_len \
-      --enc_in 321 \
-      --e_layers 1 \
+      --enc_in 9 \
+      --e_layers 3 \
       --d_model 256 \
       --dropout 0.2\
       --head_dropout 0\
       --patch_len 16\
-      --stride 8\
+      --stride 8 \
       --des 'Exp' \
-      --train_epochs 200\
-      --patience 10\
-      --lradj 'TST'\
-      --pct_start 0.2\
+      --train_epochs 100\
+      --patience 5\
       --loss_flag 2\
-      --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_sl'$seq_len'_pl'$pred_len'_random_seed_'$random_seed.log
+      --itr 1 --batch_size 256 --learning_rate 0.001 >logs/LongForecasting/$model_name'_'$model_id_name'_sl'$seq_len'_pl'$pred_len'_random_seed'$random_seed.log
 done
--- scripts/PatchTST/weather.sh
+++ scripts/PatchTST/weather.sh
@@ -14,7 +14,7 @@ model_id_name=weather
 data_name=custom
 
 random_seed=2021
-for pred_len in 96 192 336 720
+for pred_len in 192 336 720
 do
     python -u run_longExp.py \
       --random_seed $random_seed \
@@ -38,7 +38,7 @@ do
       --patch_len 16\
       --stride 8\
       --des 'Exp' \
-      --train_epochs 100\
-      --patience 20\
-      --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+      --train_epochs 200\
+      --patience 10\
+      --itr 1 --batch_size 1024 --learning_rate 0.0001 >logs/LongForecasting/$model_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
 done
